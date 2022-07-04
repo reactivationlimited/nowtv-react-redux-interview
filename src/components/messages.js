182c0261755d7b2ef7f72282cfdab8259d45764f
Jon Cooke <reactivationlimited@icloud.com>
2022-07-04 21:10:58 +0100

    render a list of messages using id as the key

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { fetchMessages } from '../reducers/messages'
-
 export const Messages = ({ status, messages}) => {
 	switch(status) {
 		case 'PENDING': return <div>Loading messages</div>
-		case 'FETCHED': return <div>{messages}</div>
+		case 'FETCHED': return <div>{
+			messages.map(({id, userId, message, timestamp}) => <div key={id}>{message}</div>)
+		}</div>
 		default: return null;
 	}
 }
--- src/components/messages.test.js
+++ src/components/messages.test.js
@@ -3,28 +3,48 @@ import { shallow } from 'enzyme';
 
 import { Messages } from './messages'
 
+
+const messages = [
+  {
+    "id": "cd445e6d-e514-424f-ba8f-16ec842002c6",
+    "userId": "fe27b760-a915-475c-80bb-7cdf14cc6ef3",
+    "message": "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+    "timestamp": "2017-02-09T04:27:38Z"
+  },
+  {
+    "id": "b03569ae-ccbf-4975-8040-4daba638b407",
+    "userId": "16373df5-da0a-4074-8295-f916b94269f4",
+    "message": "Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus.",
+    "timestamp": "2016-11-09T05:04:58Z"
+  }
+];
+
 describe('The Messages component', () => {
 	describe('when the status is PENDING', () => {
-		const wrapper = shallow(<Messages status="PENDING" messages="data"/>)
+		const wrapper = shallow(<Messages status="PENDING" messages={messages}/>)
 
 		it('should render a loading message', () => {
 			expect(wrapper.contains('Loading messages')).toBe(true);
 		})
 		it('should not render messages', () => {
-			expect(wrapper.contains('data')).toBe(false);
+			for(let message of messages) {
+				expect(wrapper.contains(message.message)).toBe(false);
+			}
 		})
 		it('should match a snapshot', () => {
 			expect(wrapper).toMatchSnapshot()
 		})
 	})
 	describe('when the status is FETCHED', () => {
-		const wrapper = shallow(<Messages status="FETCHED" messages="messages"/>)
+		const wrapper = shallow(<Messages status="FETCHED" messages={messages}/>)
 
-		it('should render messages', () => {
-			expect(wrapper.contains('messages')).toBe(true);
-		})
 		it('should not render a loading message', () => {
 			expect(wrapper.contains('Loading messages')).toBe(false);
+		})
+		it('should render messages', () => {
+			for(let message of messages) {
+				expect(wrapper.contains(message.message)).toBe(true);
+			}
 		})
 		it('should match a snapshot', () => {
 			expect(wrapper).toMatchSnapshot()
