300cc13b78b651616cb0bb196a26630bb5cb2230
Jon Cooke <reactivationlimited@icloud.com>
2022-07-05 15:13:31 +0100

    show the timestamp in a human readable format with date-fns. pressed for time now, so only snapshot test.

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { format } from 'date-fns'
 
 export const Messages = ({ status, messages}) => {
 	switch(status) {
 		case 'PENDING': return <div>Loading messages</div>
 		case 'FETCHED': return <div>{
-			messages.map(({id, userId, message, timestamp, member}) => <div title={member?.email} key={id}><img href={member?.avatar} alt="" width="100" height="100"/>{message}</div>)
+			messages.map(({id, userId, message, timestamp, member}) => 
+				<div title={member?.email} key={id}>
+					<img href={member?.avatar} alt="" width="100" height="100"/>{format(new Date(timestamp), 'do MMM yyyy HH:MM')} {message}
+				</div>
+			)
 		}</div>
 		default: return null;
 	}
