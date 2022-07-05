632016155d52936a073dc7c013da1a3bf41cf5b4
Jon Cooke <reactivationlimited@icloud.com>
2022-07-05 17:48:48 +0100

    sort messages in the selector

--- src/selectors.js
+++ src/selectors.js
@@ -1,1 +1,4 @@
-export const getMessages = state => state.messages
+export const getMessages = state => ({
+	...state.messages,
+	messages: state.messages.messages.slice(0).sort((left, right) => left.timestamp < right.timestamp ? -1 : left.timestamp === right.timestamp ? 0 : 1)
+})
--- src/selectors.test.js
+++ src/selectors.test.js
@@ -3,10 +3,35 @@ import { getMessages} from './selectors'
 
 describe('the getMessages selector', () => {
 	it('should return the value of state.messages', () => {
-		const messages = {};
+		const messages = {
+			messages: []
+		};
 		const state = {
 			messages
 		}
-		expect(getMessages(state)).toBe(messages);
+		expect(getMessages(state)).toEqual(messages);
+	})
+	it.each([
+		['', ''],
+		['a', 'a'],
+		['ab', 'ab'],
+		['ba', 'ab'],
+		['abc', 'abc'],
+		['acb', 'abc'],
+		['bac', 'abc'],
+		['bca', 'abc'],
+		['cab', 'abc'],
+		['cba', 'abc']
+	])('should sort messages lexically by timestamp', (unsorted, sorted) => {
+		const messages = {
+			messages: unsorted.split('').map(t => ({timestamp: t}))
+		};
+		const state = {
+			messages
+		}
+		const sortedMessages = {
+			messages: sorted.split('').map(t => ({timestamp: t}))
+		};
+		expect(getMessages(state)).toEqual(sortedMessages);
 	})
 })
